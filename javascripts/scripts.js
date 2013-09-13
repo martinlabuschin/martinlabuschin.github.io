d5eb63899fe4566707053a56703d86fbe9fe16e0
Martin Labuschin <labuschin@gmail.com>
2013-09-13 19:49:54 +0200

    second round

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,11 +1,21 @@
-$(document).ready(function(){
-  $(".section").hide();
-  $("#navigation a").click(function(){
-    $(".section").slideUp("fast");
-    $($(this).attr("href")).slideDown("fast");
-    $("#navigation a").removeClass("active");
-    $(this).addClass("active");
-    return false;
+jQuery(document).ready(function(){
+  jQuery(window).stellar();
+  
+  resize_height();
+  
+  $(window).resize(function(){
+    resize_height();
   });
-  $("#navigation a:first").trigger("click");
+  
+  jQuery("navigation a").click(function() {
+  		jQuery('html,body').animate({
+  		  scrollTop: jQuery(jQuery(this).attr("href")).offset().top
+  		}, 'slow');
+  	  return false;
+    });
 });
+
+function resize_height() {
+  jQuery(".masthead").css("height", ((jQuery(window).height()/100)*70)+"px");
+  jQuery("navigation ul").css("top", ((jQuery(".masthead").height()/100)*50)+"px");
+}
