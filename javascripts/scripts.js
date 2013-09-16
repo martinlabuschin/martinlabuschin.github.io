38bb381f22462c8d14232c748144c5761623845b
Martin Labuschin <labuschin@gmail.com>
2013-09-16 12:31:36 +0200

    cleanup javascript file

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,11 +1,5 @@
 jQuery(document).ready(function(){
   
-  //resize_height();
-  
-  $(window).resize(function(){
-    //resize_height();
-  });
-  
   jQuery("navigation a").click(function() {
   		jQuery('html,body').animate({
   		  scrollTop: jQuery(jQuery(this).attr("href")).offset().top
@@ -13,11 +7,3 @@ jQuery(document).ready(function(){
   	  return false;
     });
 });
-
-function resize_height() {
-  
-  total_height = ((jQuery(window).height()/100)*80);
-  
-  jQuery(".masthead .row").css("height", (total_height)+"px");
-  jQuery(".masthead .row").css("padding-top", (total_height/2)+"px");
-}
