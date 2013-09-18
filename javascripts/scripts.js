4c689c10765a8efa98fbb7058dd5b69064115471
Martin Labuschin <labuschin@gmail.com>
2013-09-18 12:23:32 +0200

    fix the scrollspy

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function(){
   
+  jQuery('body').scrollspy({ target: '#navbar-wrapper' });
+
   jQuery("navigation a").click(function() {
   		jQuery('html,body').animate({
   		  scrollTop: jQuery(jQuery(this).attr("href")).offset().top
