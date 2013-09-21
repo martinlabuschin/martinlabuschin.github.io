36f7322b304243372148de4f46fc13703ab969f1
Martin Labuschin <labuschin@gmail.com>
2013-09-21 19:31:31 +0200

    wait for image to load before displaying it

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -9,7 +9,6 @@ jQuery(document).ready(function(){
     var left = offset.left;
     jQuery("header .logo").css("left", left);
 
-    jQuery(".prologue").animate({opacity: 1, top: 0},1000);
 
     jQuery("header img").click(function() {
       jQuery('html, body').animate({
@@ -18,7 +17,13 @@ jQuery(document).ready(function(){
       return false;
     });
 
+    var _image = new Image();
+    _image.src = "/images/background.gif";
 
+    jQuery(_image).load(function () {
+      jQuery('.header_wrapper').animate({ opacity: 1 }, { duration: 1000 });
+      jQuery(".prologue").delay(1000).animate({opacity: 1, top: 0},1000);
+    });
 
     var divs = $('h1');
     $(window).scroll(function(){
