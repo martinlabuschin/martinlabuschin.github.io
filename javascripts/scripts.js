ae3d71f81aed25339cdd230748af67933b70daba
Martin Labuschin <labuschin@gmail.com>
2013-09-22 20:36:32 +0200

    refactor profile image positioning

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,17 +1,14 @@
 jQuery(document).ready(function(){
   if(!Modernizr.touch){ 
     jQuery.stellar();
-
-    var header = jQuery('.prologue');
-    var offset = header.offset();
-    var left = offset.left;
-    jQuery("header .logo").css("left", left-20);
-
-    var logo = jQuery('header img');
-    var logo_offset = logo.offset();
-    var logo_left = logo_offset.left;
-    jQuery("body").append(jQuery("header img").clone().addClass("clone").css("left", logo_left-40));
     jQuery('header img').css("opacity", 0);
+    jQuery("body").append(jQuery("header img").clone().addClass("clone"));
+
+    reposition_logo();    
+
+    jQuery(window).resize(function(){
+      reposition_logo(); 
+    });
 
     jQuery("header img, .clone").click(function() {
       jQuery('html, body').animate({
@@ -24,8 +21,8 @@ jQuery(document).ready(function(){
     _image.src = "/images/background.gif";
 
     jQuery(_image).load(function () {
-      jQuery('.header_wrapper').delay(1000).animate({ opacity: 1 }, { duration: 1000 });
-      jQuery(".prologue").delay(1000).animate({opacity: 1, top: 0},1000);
+      jQuery('.header_wrapper').animate({ opacity: 1 }, { duration: 1000 });
+      jQuery(".prologue").delay(500).animate({opacity: 1, top: 0},1000);
     });
 
     var divs = $('h1');
@@ -38,3 +35,15 @@ jQuery(document).ready(function(){
     });
   } 
 });
+
+function reposition_logo() {
+  var header = jQuery('.prologue');
+  var offset = header.offset();
+  var left = offset.left;
+  jQuery("header .logo").css("left", left-20);
+  var logo = jQuery('header img');
+  var logo_offset = logo.offset();
+  var logo_left = logo_offset.left;
+  jQuery(".clone").css("left", logo_left-40);
+  jQuery(".clone").css("opacity", 1);
+}
