fedcf92edaa9eb95c14524d4eec27bfe9551e4e1
Martin Labuschin <labuschin@gmail.com>
2013-09-22 13:33:55 +0200

    reposition profile image

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,18 +1,16 @@
 jQuery(document).ready(function(){
   if(!Modernizr.touch){ 
     jQuery.stellar();
-    
 
-    var header = jQuery('header');
+    var header = jQuery('.prologue');
     var offset = header.offset();
     var left = offset.left;
-    jQuery("header .logo").css("left", left);
-
+    jQuery("header .logo").css("left", left-20);
 
     var logo = jQuery('header img');
-    var logo_offset = header.offset();
+    var logo_offset = logo.offset();
     var logo_left = logo_offset.left;
-    jQuery("body").append(jQuery("header img").clone().addClass("clone").css("left", logo_left+20));
+    jQuery("body").append(jQuery("header img").clone().addClass("clone").css("left", logo_left));
     jQuery('header img').css("opacity", 0);
 
     jQuery("header img, .clone").click(function() {
@@ -26,7 +24,7 @@ jQuery(document).ready(function(){
     _image.src = "/images/background.gif";
 
     jQuery(_image).load(function () {
-      jQuery('.header_wrapper').animate({ opacity: 1 }, { duration: 1000 });
+      jQuery('.header_wrapper').delay(1000).animate({ opacity: 1 }, { duration: 1000 });
       jQuery(".prologue").delay(1000).animate({opacity: 1, top: 0},1000);
     });
 
